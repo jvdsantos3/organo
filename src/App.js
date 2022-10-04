f664dd6381332ee4f2e2e79d2078b66a0c7b5b5a
João Vitor <jv.dossantos012@gmail.com>
2022-10-04 19:48:49 -0300

    Banner do Organo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Banner />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
